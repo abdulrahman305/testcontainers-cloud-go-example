6699c5f98d074799038463b7cca8dadd10914c58
Eddú Meléndez Gonzales <eddu.melendez@gmail.com>
2023-09-05 15:34:23 -0600

    Assert query result (#8)
    Co-authored-by: Manuel de la Peña <social.mdelapenya@gmail.com>

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/AtomicJar/testcontainers-cloud-go-example
 go 1.19
 
 require (
+	github.com/lib/pq v1.10.9
 	github.com/stretchr/testify v1.8.4
 	github.com/testcontainers/testcontainers-go v0.23.0
 	github.com/testcontainers/testcontainers-go/modules/postgres v0.23.0
+	gotest.tools/v3 v3.5.0
 )
 
 require (
@@ -22,6 +24,7 @@ require (
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/klauspost/compress v1.16.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
--- main_test.go
+++ main_test.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"path/filepath"
 	"strings"
 	"testing"
 	"time"
 
+	_ "github.com/lib/pq"
 	"github.com/stretchr/testify/require"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
@@ -29,7 +32,26 @@ func TestTestcontainersCloud(t *testing.T) {
 				WithOccurrence(2).WithStartupTimeout(10*time.Second)),
 	)
 	require.NoError(t, err)
-	defer c.Terminate(ctx)
+	t.Cleanup(func() {
+		if err := c.Terminate(ctx); err != nil {
+			t.Fatalf("failed to terminate container: %s", err)
+		}
+	})
+
+	cs, err := c.ConnectionString(ctx, "sslmode=disable")
+	require.NoError(t, err)
+
+	db, err := sql.Open("postgres", cs)
+	defer db.Close()
+
+	var numberOfGuides int
+	result, err := db.Query("SELECT COUNT(*) FROM guides")
+	defer result.Close()
+	require.NoError(t, err)
+
+	result.Next()
+	result.Scan(&numberOfGuides)
+	assert.Equal(t, numberOfGuides, 6)
 
 	dockerClient, err := testcontainers.NewDockerClient()
 	require.NoError(t, err)
